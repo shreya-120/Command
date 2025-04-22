d63f563694b970e2573780ce15c7539da65f76b8
Shreya-120 <shreyasabannavar320@gmail.com>
2025-04-22 14:18:25 +0530

    first commit

---
+++ Student.java
@@ -0,0 +1,13 @@
+public class Student {
+    public static void main(String[] args) {
+        String name="Deepa";
+        int id=124;
+        String course="Devops";
+        String branch="BCA";
+
+        System.out.println("Student Name:"+name);
+        System.out.println("Student Id:"+id);
+        System.out.println("Student Course:"+course);
+        System.out.println("Student Branch:"+branch);
+    }
+}
